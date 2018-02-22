6670879e68cb2af170e3f0c8b6171841fd637596
Jas <jazjit.singh@gmail.com>
2018-02-21 22:13:12 -0500

    updates

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://jas:jas@ds143738.mlab.com:43738/jas-node-restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
